dc3fbad0789f151cb37a112a8f54e2eff4435a8f
Tomek Stacharski <twtstacharski@gmail.com>
2020-03-28 12:20:26 +0000

    routed to restaurants from homepage for now:

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :vouchers, only: [:index, :show, :new, :create]
   resources :restaurants, only: [:index, :show]
   devise_for :users
-  root to: 'pages#home'
+  # root to: 'pages#home'
+  root to: "restaurants#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

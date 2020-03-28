93ab5215a7a226780ae2001f9756b09d9bce41be
Tomek Stacharski <twtstacharski@gmail.com>
2020-03-28 12:16:56 +0000

    can view vouchers from restaurants view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :vouchers, only: [:index, :show, :new, :create]
-  # resources :restaurants
+  resources :restaurants, only: [:index, :show]
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

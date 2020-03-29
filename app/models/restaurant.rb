3c1128d78c0f3cb2a7391bb8ac2967b45a0d46c1
Tomek Stacharski <twtstacharski@gmail.com>
2020-03-29 18:04:50 +0100

    added photo url to restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -13,24 +13,15 @@ class Restaurant < ApplicationRecord
       geocoder_search = geocoder_search.first
     end
 
-    # STOP HERE
-    # if geocoder_search
-    # binding.pry
-      place_id = geocoder_search.data["place_id"]
-      self.google_place_id = place_id
-    # end
+    place_id = geocoder_search.data["place_id"]
+    google_data_url = "https://maps.googleapis.com/maps/api/place/details/json?placeid=#{place_id}&key=#{ENV['GOOGLE_API_SERVER_KEY']}"
+    google_data = JSON.parse(open(google_data_url).read)
+    photo_reference = google_data["result"]["photos"][0]["photo_reference"] || ""
+    google_photo_url = "https://maps.googleapis.com/maps/api/place/photo?photoreference=#{photo_reference}&sensor=false&maxheight=1200&maxwidth=1200&key=#{ENV['GOOGLE_API_SERVER_KEY']}"
 
-    # url = "https://maps.googleapis.com/maps/api/place/details/json?placeid=#{place_id}&key=#{ENV['GOOGLE_API_SERVER_KEY']}"
-    # user_serialized = open(url).read
-    # datas = JSON.parse(user_serialized)
-    # self.website = datas["result"]["website"] unless datas["result"].nil?
-    # photo_reference = datas["result"]["photos"][0]["photo_reference"] unless datas["result"].nil?
-
-    # photoUrl = "https://maps.googleapis.com/maps/api/place/photo?photoreference=#{photo_reference}&sensor=false&maxheight=1200&maxwidth=1200&key=#{ENV['GOOGLE_API_SERVER_KEY']}"
-# binding.pry
+    self.google_place_id = place_id
+    self.website = google_data["result"]["website"] || ""
+    self.photo_url = google_photo_url
     self.save
-    # binding.pry
   end
-
-    # STOPHERE
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_29_164932) do
+ActiveRecord::Schema.define(version: 2020_03_29_165518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_03_29_164932) do
     t.string "website"
     t.string "google_place_id"
     t.string "country"
+    t.string "photo_url"
   end
 
   create_table "users", force: :cascade do |t|

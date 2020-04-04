a032c48981675db383d548d1d4571a19d32a5fea
Tomek Stacharski <twtstacharski@gmail.com>
2020-04-04 13:31:14 +0100

    tidied up voucher model

--- app/models/voucher.rb
+++ app/models/voucher.rb
@@ -8,12 +8,7 @@ class Voucher < ApplicationRecord
   def image_from_google
     restaurant = self.restaurant
     google_file = URI.open(restaurant.google_photos(restaurant.google_place_id, 400).sample)
-    # STOPHERE
     self.photo.attach(io: google_file, filename: `#voucher_{id}.png`, content_type: 'image/png')
     self.save
   end
 end
-
-# file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
-# article = Article.new(title: 'NES', body: "A great console")
-# article.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')

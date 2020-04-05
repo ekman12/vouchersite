0947c2510d475a957c15b947bd4fb3023521996a
Tomek Stacharski <twtstacharski@gmail.com>
2020-04-05 12:20:29 +0100

    limited search when uploading to only UK

--- app/javascript/packs/autocomplete.js
+++ app/javascript/packs/autocomplete.js
@@ -1,8 +1,12 @@
 const autocomplete = () => {
   console.log('is it here?')
     var post_address = document.getElementById('voucher_restaurant');
+    var options = {
+      types: ['establishment'],
+      componentRestrictions: {country: "uk"}
+    };
     if (post_address) {
-      var autocomplete = new google.maps.places.Autocomplete(post_address, { types: [ 'establishment' ] });
+      var autocomplete = new google.maps.places.Autocomplete(post_address, options);
       google.maps.event.addDomListener(post_address, 'keydown', function(e) {
         if (e.key === "Enter") {
           e.preventDefault(); // Do not submit the form on Enter.

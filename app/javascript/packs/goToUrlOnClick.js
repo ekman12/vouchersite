cf27b9bfc92cf19b8ad045c31fa6961575aba12f
Tomek Stacharski <twtstacharski@gmail.com>
2020-04-05 13:03:06 +0100

    added on click for vouchers

--- app/javascript/packs/goToUrlOnClick.js
+++ app/javascript/packs/goToUrlOnClick.js
@@ -1,12 +1,9 @@
 const goToUrlOnClick = () => {
-  let divs = document.querySelectorAll('.clickable-card')
-
-  divs.forEach(div => {
+  document.querySelectorAll('.clickable-card').forEach(div => {
     div.addEventListener('click', ()=>{
-      // console.log(div.getAttribute('data-id'))
-      // console.log(div.getAttribute('data-card'))
       window.location = `/${div.getAttribute('data-card')}s/${div.getAttribute('data-id')}`
     });
   })
 }
+
 export { goToUrlOnClick }
